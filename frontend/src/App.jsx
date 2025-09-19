3c426f2483bdae6549415f00516437fa7ae7766e
ASH <ayushsinghd49@gmail.com>
2025-09-19 21:59:01 +0530

    deleted

--- frontend/src/App.jsx
+++
@@ -1,7 +0,0 @@
-// src/App.jsx
-import React from "react";
-import AnalystDashboard from "./pages/AnalystDashboard.jsx"; // <-- import the component
-
-export default function App() {
-  return <AnalystDashboard />; // now this will work
-}
